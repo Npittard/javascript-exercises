b0da46838edcfdf6cd6d085ea1a63f324fd8f80a
Nathan Pittard <pittardnm@gmail.com>
2022-05-26 08:30:07 -0400

    .

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const repeatString = function(string,num) {
     let newString = '';
     if(num <0){
@@ -13,19 +12,3 @@ const repeatString = function(string,num) {
 
 // Do not edit below this line
 module.exports = repeatString;
-=======
-const repeatString = function(string,num) {
-    let newString = '';
-    if(num <0){
-        newString = 'ERROR'
-    }else {
-        for (let i = 1; i <= num; i++){
-            newString = newString.concat(string)
-        }
-    }
-    return newString
-};
-
-// Do not edit below this line
-module.exports = repeatString;
->>>>>>> 1feaf175fd2f9a666f37f1d764506b6fdb57bd68
